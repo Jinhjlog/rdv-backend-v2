6f08c4c5e0c2b4ad0ed85c130c1bca233783b6a1
Jinhjlog <dndn2615@gmail.com>
2026-01-17 17:46:25 +0900

    🔧 module: 사용자 - 로그인 기능 모듈 의존성 설정
    - JwtModule import 추가
    - LoginUseCase provider 등록
    - AuthService provider 등록
    - 토큰 발급 및 인증 기능 모듈 완성

--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -1,18 +1,25 @@
 import { Module, Provider } from '@nestjs/common';
 import { UserRepositoryImpl } from './infra/repositories';
-import { CheckAccountExistsUseCase } from './application/usecases';
+import {
+  CheckAccountExistsUseCase,
+  LoginUseCase,
+} from './application/usecases';
 import { AuthController } from './presentation/controllers';
 import { UserRepository } from './domain/repositories';
+import { JwtModule } from '@core/jwt/jwt.module';
+import { AuthService } from './domain/services/auth.service';
 
-const useCases: Provider[] = [CheckAccountExistsUseCase];
+const useCases: Provider[] = [CheckAccountExistsUseCase, LoginUseCase];
 
 @Module({
+  imports: [JwtModule],
   controllers: [AuthController],
   providers: [
     {
       provide: UserRepository,
       useClass: UserRepositoryImpl,
     },
+    AuthService,
     ...useCases,
   ],
 })
