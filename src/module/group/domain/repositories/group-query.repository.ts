fc36471572c23ef7517f4c444749ab16f3e7eac3
Jinhjlog <dndn2615@gmail.com>
2026-01-17 19:15:35 +0900

    ✨ domain: 그룹 - 그룹 조회 쿼리 모델 및 저장소 인터페이스 추가

---
+++ src/module/group/domain/repositories/group-query.repository.ts
@@ -0,0 +1,24 @@
+import { GroupListItemQueryModel, GroupDetailQueryModel } from '../models';
+
+export interface FindGroupListParams {
+  contextUserId?: string;
+}
+
+export interface FindGroupDetailParams {
+  groupId: string;
+}
+
+/**
+ * Group 조회용 Repository
+ *
+ * 복잡한 조회 쿼리를 처리합니다.
+ */
+export abstract class GroupQueryRepository {
+  abstract findList(
+    params: FindGroupListParams,
+  ): Promise<GroupListItemQueryModel[]>;
+
+  abstract findDetail(
+    params: FindGroupDetailParams,
+  ): Promise<GroupDetailQueryModel | undefined>;
+}
