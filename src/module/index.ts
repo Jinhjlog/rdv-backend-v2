8559057d85779cbf17a0d848cc08ca2114124cc2
Jinhjlog <dndn2615@gmail.com>
2026-01-17 17:10:57 +0900

    🔧 module: 사용자 모듈 의존성 설정
    - UserModule 생성 및 의존성 설정
    - CheckAccountExistsUserUseCase provider 등록
    - CoreModule import 추가
    - app.module에 UserModule 등록

--- src/module/index.ts
+++ src/module/index.ts
@@ -2,5 +2,11 @@ import { CoreModule } from '@core/core.module';
 import { Type } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
+import { UserModule } from './user/user.module';
 
-export const modules: Type<any>[] = [CoreModule, AuthModule, HealthModule];
+export const modules: Type<any>[] = [
+  CoreModule,
+  AuthModule,
+  HealthModule,
+  UserModule,
+];
