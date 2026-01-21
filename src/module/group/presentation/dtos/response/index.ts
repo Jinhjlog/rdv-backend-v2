456cca837be701a70f67761c60398a865c1469f1
Jinhjlog <dndn2615@gmail.com>
2026-01-21 22:13:48 +0900

    ✨ presentation: 모임 - 멤버 출석 통계 조회 API 엔드포인트 구현
    - GET /groups/:groupId/attendance-statistics 엔드포인트 추가
    - GroupMemberAttendanceStatisticsResponseDto 추가
    - 모임별 멤버 출석 통계 조회 API
    - Swagger 문서화 (멤버별 도착/지각/부재/출석률)

--- src/module/group/presentation/dtos/response/index.ts
+++ src/module/group/presentation/dtos/response/index.ts
@@ -2,3 +2,4 @@ export * from './group-detail.response.dto';
 export * from './group-list.response.dto';
 export * from './create-invite-code.response.dto';
 export * from './send-short-talk-message.response.dto';
+export * from './group-member-attendance-statistics.response.dto';
