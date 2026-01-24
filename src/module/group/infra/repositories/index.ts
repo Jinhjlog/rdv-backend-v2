8b0f170622435e3427c0c29d6178a2ddc5e56ab6
Jinhjlog <dndn2615@gmail.com>
2026-01-24 23:13:22 +0900

    ✨ infra: 숏톡 - 발신자 정보 조회 레포지토리 구현
    - ShortTalkUserQueryRepositoryImpl 구현
    - Prisma를 통한 사용자 정보 조회 로직 추가

--- src/module/group/infra/repositories/index.ts
+++ src/module/group/infra/repositories/index.ts
@@ -4,3 +4,4 @@ export * from './invite-code.repository.impl';
 export * from './short-talk-session.repository.impl';
 export * from './chat-message.repository.impl';
 export * from './chat-message-query.repository.impl';
+export * from './short-talk-user-query.repository.impl';
