88103432ec022f157dfe794393a34b3d396b33b7
Jinhjlog <dndn2615@gmail.com>
2026-05-25 16:20:05 +0900

    ♻️ domain: Short-Talk 도메인 레이어 group → short-talk 모듈로 이전
    - ChatMessage, ShortTalkSession 모델을 group 도메인에서 분리
    - ChatMessageRepository, ShortTalkSessionRepository 인터페이스 이전
    - ChatMessageQueryService, ShortTalkUserQueryService 인터페이스 이전
    - GroupMembershipLookupService 추상 클래스 신규 추가
    - group 도메인 index에서 short-talk 관련 export 제거

--- src/module/group/domain/models/index.ts
+++ src/module/group/domain/models/index.ts
@@ -5,11 +5,3 @@ export * from './group/group-member-attendance-statistics.read-model';
 
 export * from './invite-code/invite-access-code';
 export * from './invite-code/invite-code';
-
-export * from './short-talk/short-talk-session';
-export * from './short-talk/short-talk-listener';
-export * from './short-talk/short-talk-event';
-export type { SseMessageEvent } from './short-talk/short-talk-event';
-
-export * from './chat-message/chat-message';
-export * from './chat-message/chat-message.read-model';
--- src/module/group/domain/services/index.ts
+++ src/module/group/domain/services/index.ts
@@ -1,5 +1,3 @@
 export * from './group-leave-policy.service';
 export * from './group-query.service';
-export * from './chat-message-query.service';
-export * from './short-talk-user-query.service';
 export * from './event-lookup.service';
